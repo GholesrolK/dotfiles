df20dba31dc86fbb2e9ff09f6418dc57fd622349
GholesrolK <aelwa3r17@gmail.com>
2023-11-25 19:46:31 +0200

    first commit

---
+++ dooit/config.py
@@ -0,0 +1,4 @@
+BACKGROUND = "#000000" # background color of the app
+BAR_BACKGROUND = "#000000"  # background color of bar
+WORKSPACES_BACKGROUND = "#000000"  # background color for workspaces pane
+TODOS_BACKGROUND = "#000000"  # background color for todos pane
